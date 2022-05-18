4502fdede40f13423ca2539f777beeb96346f2da
Denis Popkov <pkvdenis@gmail.com>
2022-05-18 17:18:23 +0300

    Add project.

---
+++ src/main/java/learn/springang/umsportalrestassured/UmsportalRestassuredApplication.java
@@ -0,0 +1,13 @@
+package learn.springang.umsportalrestassured;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UmsportalRestassuredApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UmsportalRestassuredApplication.class, args);
+    }
+
+}
---
+++ src/test/java/learn/springang/umsportalrestassured/UmsportalRestassuredApplicationTests.java
@@ -0,0 +1,13 @@
+package learn.springang.umsportalrestassured;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UmsportalRestassuredApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
